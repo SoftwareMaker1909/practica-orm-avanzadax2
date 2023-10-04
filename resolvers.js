1c161ff288e69eacf6317a4793f14a8a1cbb3246
SoftwareMaker1909 <105822309+SoftwareMaker1909@users.noreply.github.com>
2023-10-04 10:42:06 -1100

    Create resolvers.js

---
+++ resolvers.js
@@ -0,0 +1,39 @@
+const resolvers = {
+  Query: {
+    // Resolver para la consulta 'inventory'
+    inventory: async () => {
+      // Aquí implementa la lógica para obtener todos los inventarios.
+      // Puedes usar consultas a la base de datos u otras fuentes de datos.
+      // Debes devolver un array de objetos que coincidan con el tipo 'Inventory'.
+    },
+
+    // Resolver para la consulta 'inventoryByFlavor'
+    inventoryByFlavor: async (parent, { flavor }) => {
+      // Implementa la lógica para obtener inventarios por sabor.
+      // El argumento 'flavor' contiene el sabor proporcionado en la consulta.
+      // Debes devolver un array de objetos 'Inventory' que coincidan con el sabor.
+    },
+
+    // Resolver para la consulta 'inventoryByShop'
+    inventoryByShop: async (parent, { shop }) => {
+      // Implementa la lógica para obtener inventarios por tienda.
+      // El argumento 'shop' contiene el nombre de la tienda proporcionado en la consulta.
+      // Debes devolver un array de objetos 'Inventory' que coincidan con la tienda.
+    },
+
+    // Resolver para la consulta 'inventoryByDate'
+    inventoryByDate: async (parent, { date }) => {
+      // Implementa la lógica para obtener inventarios por fecha.
+      // El argumento 'date' contiene la fecha proporcionada en la consulta.
+      // Debes devolver un array de objetos 'Inventory' que coincidan con la fecha.
+    },
+  },
+  Mutation: {
+    // Resolver para la mutación 'addInventory'
+    addInventory: async (parent, { inventory }) => {
+      // Implementa la lógica para agregar un nuevo elemento al inventario.
+      // El argumento 'inventory' contiene los datos proporcionados en la mutación.
+      // Debes realizar la inserción en tu fuente de datos y devolver el nuevo 'Inventory'.
+    },
+  },
+};
